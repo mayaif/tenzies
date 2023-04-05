2182a7dd96e1f462f457063fc33e24616ad6cfd1
mayaif <margareta.ifrim@outlook.com>
2023-04-05 15:08:07 +0100

    refactor

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App() {
   return (
     <main >
       {tenzies && <Confetti />}
-      <h1 className="title">Tenzies</h1>
+      <h1 className="title">Tenzi</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diceEmements}
